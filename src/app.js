ffa5f60ec35c532e3f664cfa9e092e8557e43a50
Lucas <Lucas>
2020-06-30 13:55:31 -0300

    Desafio Node JS
    Desafio concluído com todos os testes realizados e aprovado.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -10,24 +11,83 @@ app.use(cors());
 
 const repositories = [];
 
+//List All Repositories ok
 app.get("/repositories", (request, response) => {
-  // TODO
+    return response.json(repositories);
 });
 
+//Create New Repository ok
 app.post("/repositories", (request, response) => {
-  // TODO
+   const {title, url, techs} = request.body;
+
+   const repository = {
+     id: uuid(),
+     title, 
+     url,
+     techs,
+     likes: 0,
+   };
+
+   repositories.push(repository);
+   return response.json(repository);
+
 });
 
+//Update Repository
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json();
+  }
+
+    const updatedRepository = { 
+      ...repositories[repositoryIndex],
+      title, 
+      url,
+      techs
+  };
+
+  repositories[repositoryIndex] = updatedRepository;
+
+  return response.json(updatedRepository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
-
+//Crete New Like ok
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repository = repositories.find( repository => repository.id === id);
+  
+
+  if(!repository){
+    return response.status(400).json({ error: 'Project not found'})
+}
+
+  repository.likes ++;
+
+  return response.json(repository);
 });
+
+//Delete Repository. ok
+app.delete("/repositories/:id", (request, response) => {
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex( repository => repository.id === id);
+
+    if(repositoryIndex < 0){
+      return response.status(400).json({ error: 'Project not found'})
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+   
+});
+
+
 
 module.exports = app;
